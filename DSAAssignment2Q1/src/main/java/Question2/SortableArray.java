555b9582320f6221dce49c0e2d5b165a7f66bccc
Shawn Hiew <yvn7103@autuni.ac.nz>
2022-10-10 10:00:14 +1300

    q2 updates
    written comments

--- DSAAssignment2Q1/src/main/java/Question2/SortableArray.java
+++ DSAAssignment2Q1/src/main/java/Question2/SortableArray.java
@@ -12,23 +12,29 @@ public class SortableArray<E extends Comparable> {
 
     E[] array;
 
+    //Sets an array
     public void setArray(E[] array) {
         this.array = array;
     }
 
+    //Calls quickSort function with inputs
     public void quickSort() {
         quickSort(array, 0, array.length - 1);
     }
 
+    //QuickSort function that if the first item is smaller than the last item calls partition function
     public void quickSort(E arr[], int begin, int end) {
         if (begin < end) {
             int index = partition(arr, begin, end);
 
+            //Sorts for the beginning to index-1
             quickSort(arr, begin, index - 1);
+            //Sorts for p+1 to the end
             quickSort(arr, index + 1, end);
         }
     }
 
+    //Function that sort through the array and calls the swap function
     public int partition(E[] arr, int low, int high) {
         int p = low, j;
         for (j = low + 1; j <= high; j++) {
@@ -41,6 +47,7 @@ public class SortableArray<E extends Comparable> {
         return p;
     }
 
+    //Executes swapping of items in the array
     public void swap(E[] arr, int low, int pivot) {
         E tmp = arr[low];
         arr[low] = arr[pivot];

8da9bb20f13cf0f2e32cf9ed35d25f35285bee95
Shawn Hiew <yvn7103@autuni.ac.nz>
2022-10-09 18:01:07 +1300

    Added Functionalities to buttons
    Some buttons work

---
+++ DSAAssignment2Q1/src/main/java/Question1/FileReaderWriter.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Question1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author shawn
+ */
+public class FileReaderWriter {
+
+    public String name;
+    public String[] lineData;
+    public int lineNumber;
+
+    public FileReaderWriter() {
+        lineData = new String[1000];
+    }
+
+    public void readFile(String fileName) {
+        File f = new File(fileName);
+        try {
+            Scanner myScanner = new Scanner(f);
+            lineNumber = 0;
+            while (myScanner.hasNextLine()) {
+                String line = myScanner.nextLine();
+                lineData[lineNumber] = line;
+                lineNumber++;
+            }
+            myScanner.close();
+        } catch (IOException e) {
+            System.out.println("Cannot read the file " + e.getMessage());
+        }
+    }
+
+    //System.getProperty("line.separator");
+    public void writeFile(String fileName, String c) {
+        File f = new File(fileName);
+
+        try {
+            f.createNewFile();
+            FileWriter writer = new FileWriter(f);
+            writer.write(c);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Cannot write to the file" + e.getMessage());
+        }
+    }
+}
--- DSAAssignment2Q1/src/main/java/Question1/Gui.java
+++ DSAAssignment2Q1/src/main/java/Question1/Gui.java
@@ -5,11 +5,20 @@
 package Question1;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
@@ -21,8 +30,10 @@ public class Gui {
     JPanel panel;
     JButton loadButton;
     JButton saveButton;
-    JOptionPane sortOrder;
+    JButton sortOrder;
     JButton searchStudent;
+    JTextField jTFattachString;
+    FileReaderWriter fileRW;
 
     public Gui() {
         //Setting up Frame
@@ -32,15 +43,63 @@ public class Gui {
         f.setBackground(Color.GRAY);
         f.setDefaultCloseOperation(EXIT_ON_CLOSE);
         f.getContentPane();
-        f.setVisible(true);
 
         //Setting up panel
         panel = new JPanel();
         panel.setBackground(Color.GRAY);
+        GridBagLayout layout = new GridBagLayout();
+        panel.setLayout(layout);
 
+        //Load File Button
         loadButton = new JButton("Load");
+        loadButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                JFileChooser txtFileChooser = new JFileChooser(new File("."));
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("", "txt");
+                txtFileChooser.setFileFilter(filter);
+                int stateTxtFileChooser = txtFileChooser.showOpenDialog(null);
+
+                if (stateTxtFileChooser == JFileChooser.APPROVE_OPTION) {
+                    String fileName = txtFileChooser.getSelectedFile().getPath();
+                    fileRW.readFile(fileName);
+                }
+            }
+        });
+
+        //Save File Button
         saveButton = new JButton("Save");
-        sortOrder = new JOptionPane("Sorting Order");
+        saveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                String outputFileContent = "";
+                for (int i = 0; i < fileRW.lineNumber; i++) {
+                    outputFileContent += (fileRW.lineData[i] + " " + jTFattachString.getText() + "\n");
+                }
+
+                JFileChooser txtFileChooser = new JFileChooser(new File("output.txt"));
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("", "txt");
+                txtFileChooser.setFileFilter(filter);
+                int stateTxtFileChooser = txtFileChooser.showSaveDialog(null);
+                if (stateTxtFileChooser == JFileChooser.APPROVE_OPTION) {
+                    String fileName = txtFileChooser.getSelectedFile().getPath();
+                    fileRW.writeFile(fileName, outputFileContent);
+                }
+            }
+        });
+
+        //Sorting Order of Displayed results
+        sortOrder = new JButton("Sorting Order");
+        sortOrder.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                String[] sortingOrder = new String("By Name Ascending, By Name Descending, By Marks Ascending, By Marks Descending").split(", ");
+                String messageInput;
+                messageInput = (String) JOptionPane.showInputDialog(null, "Sorting Order", "Sorting Order", JOptionPane.PLAIN_MESSAGE, null, sortingOrder, null);
+                if (messageInput.equalsIgnoreCase("By Name Ascending")) {
+                    jTFattachString.setText(messageInput);
+                }
+            }
+        });
+
+        //Student Search
         searchStudent = new JButton("Search");
 
         panel.add(loadButton);
@@ -48,5 +107,7 @@ public class Gui {
         panel.add(sortOrder);
         panel.add(searchStudent);
         f.add(panel);
+        f.setVisible(true);
+        fileRW = new FileReaderWriter();
     }
 }
